bb4a61228f7c9c0ad56a60248e4cadbb80c38d9b
jihanebs <jihanebensalem023@gmail.com>
2026-01-24 20:50:46 +0100

    Commit
    commit components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
-import Message from "./Message";
-
+import Alert from "./components/Alert";
+//import ListGroup from "./components/ListGroup";
 
 function App() {
-return <div> <Message /></div>
+ /* let items = ["New York", "San Fransisco", "Tokyo", "London", "Paris"];
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+  return (
+    <div>
+      <ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem} />
+    </div>
+  );*/
+  return(
+    <div>
+        <Alert>
+            Hello world
+            <Alert/>
+    </div>
+  );
 }
- export default App;
+export default App;
