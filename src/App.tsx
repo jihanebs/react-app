781275d9e0a85f294e3b6f25ef6063a2f64baa4a
jihanebs <jihanebensalem023@gmail.com>
2026-01-28 16:47:06 +0100

    alert close

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,18 @@
+import { useState } from "react";
 import Alert from "./components/Alert";
+import Button from "./components/Button";
 //import ListGroup from "./components/ListGroup";
 
 function App() {
- /* let items = ["New York", "San Fransisco", "Tokyo", "London", "Paris"];
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
-  return (
-    <div>
-      <ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem} />
-    </div>
-  );*/
+    const [alertVisible, setAlertVisibility]=useState(false)
+
   return(
     <div>
-        <Alert>
-          Hello <span>World</span>  
-        </Alert>
+        {alertVisible
+        && <Alert onClose={()=> setAlertVisibility(false)}>
+            MY ALERT
+        </Alert>}
+        <Button color ="primary" onClick={() => setAlertVisibility(true)}>My button</Button>
     </div>
   );
 }
